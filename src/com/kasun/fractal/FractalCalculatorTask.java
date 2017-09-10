7d215caa7e5f8dd1451b5a1910c30d6a1f5864b8
kasun <alankasun@gmail.com>
2017-09-10 17:43:02 +0530

    added interrupts

--- src/com/kasun/fractal/FractalCalculatorTask.java
+++ src/com/kasun/fractal/FractalCalculatorTask.java
@@ -21,18 +21,6 @@ public class FractalCalculatorTask implements Runnable {
         this.callback = callback;
     }
 
-
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
     @Override
     public void run() {
         if (!Thread.currentThread().isInterrupted()) {
