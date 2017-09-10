26d232988a66455651e2a47ff4950c317f8044fe
kasun <alankasun@gmail.com>
2017-09-10 17:56:54 +0530

    remove jform eval

--- src/com/kasun/ui/PanelManderboltPropeties.java
+++ src/com/kasun/ui/PanelManderboltPropeties.java
@@ -13,6 +13,18 @@ import javax.swing.*;
  * @author Kasun Vithanage
  */
 public class PanelManderboltPropeties extends JPanel implements FractalProperties {
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Kasun Vithanage
+    private JPanel panel1;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JLabel label4;
+    private JTextField textFieldRealMin;
+    private JTextField textFieldRealMax;
+    private JTextField textFieldImgMin;
+    private JTextField textFieldImgMax;
+    private JLabel label5;
     public PanelManderboltPropeties() {
         initComponents();
     }
@@ -33,20 +45,6 @@ public class PanelManderboltPropeties extends JPanel implements FractalPropertie
 
         //======== panel1 ========
         {
-
-            // JFormDesigner evaluation mark
-            panel1.setBorder(new javax.swing.border.CompoundBorder(
-                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-                            "JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-                            javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-                            java.awt.Color.red), panel1.getBorder()));
-            panel1.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
-                public void propertyChange(java.beans.PropertyChangeEvent e) {
-                    if ("border".equals(e.getPropertyName())) throw new RuntimeException();
-                }
-            });
-
-
             //---- label1 ----
             label1.setText("Real Min :");
             label1.setLabelFor(textFieldRealMin);
@@ -123,19 +121,6 @@ public class PanelManderboltPropeties extends JPanel implements FractalPropertie
 
         add(panel1);
     }
-
-    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-    // Generated using JFormDesigner Evaluation license - Kasun Vithanage
-    private JPanel panel1;
-    private JLabel label1;
-    private JLabel label2;
-    private JLabel label3;
-    private JLabel label4;
-    private JTextField textFieldRealMin;
-    private JTextField textFieldRealMax;
-    private JTextField textFieldImgMin;
-    private JTextField textFieldImgMax;
-    private JLabel label5;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 
     @Override
